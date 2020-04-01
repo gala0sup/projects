05984daa099d78d5efae9e5ad7b7fb47dc515883
CaptianLevi <solankivibhakar82@gmail.com>
2020-04-01 15:01:55 +0530

    download_class complete for one website

--- PYTHON/manga_download/scripts/downloader_class.py
+++ PYTHON/manga_download/scripts/downloader_class.py
@@ -1,5 +1,13 @@
-import requests 
+import base64
+import os
+import pathlib
+import re
+import shutil
+import string
+
+import requests
 from bs4 import BeautifulSoup
+
 
 class Downloader(object):
     
@@ -12,8 +20,11 @@ class Downloader(object):
         self.name = None
         self.chapter_list = {}
         self.total_chapters = None
+        self.chapter_names = {}
         self.last_chapter = None
+        
         self.download_dir = None
+        self.image_links= []
 
     def get_info(self,manga_link):
 
@@ -27,9 +38,12 @@ class Downloader(object):
 
         self.prased_html =BeautifulSoup(self.page.text,'lxml')
 
-    def _get_webpage(self,page_link):
+    def _get_webpage(self,page_link,_stream=False,_raw=False):
 
-        self.page = requests.get(page_link)
+        if _raw:
+            self.page = requests.get(page_link,stream=_stream).raw
+        else:
+            self.page = requests.get(page_link,stream=_stream)
 
     def status_code(self):
         return self.page.status_code
@@ -44,8 +58,10 @@ class Downloader(object):
 
         for chapter_link in chapter_list:
             link = str(chapter_link.get('href'))
+            chapter_name = str(chapter_link.get('title'))
             chapter_number = link.split('/')[-1]
-            self.chapter_list.update({str(chapter_number):str(link)})
+            self.chapter_list.update({str(chapter_number) : str(link)})
+            self.chapter_names.update({str(chapter_number) : str(chapter_name)})
 
         self.total_chapters = len(self.chapter_list.keys())
         self.last_chapter = list(self.chapter_list.keys())[0]
@@ -62,47 +78,107 @@ class Downloader(object):
         }
         
     def download(self,link=None):
-        choice = input('choose option \n1. download all \n2.download from list')
+        choice = input('choose option \n1. download all \n2.download from list \n')
         if self.download_dir != None:
             if choice==1:
-                for chapter_number,chapter_link in reversed(self.chapter_list.items()):
+                for chapter_number in reversed(self.chapter_list.keys()):
     
                     print("downloading {}".format(chapter_number))
 
                     self._get_webpage(self.chapter_list[chapter_number])
                     self._make_soup()
+                    self._get_image_links()
 
-                    chapter_reader = (self.prased_html.find(class_='container-chapter-reader')).findAll('img')
+                    for link in self.image_links:
+                        self._download_image(link,chapter_number)
 
-                    image_links = []
+            else:
+                while True:
+
+                    while True:
+
+                        print('choose from chapter list\n')
+                        print('chapter number : chapter name')
+                        for i in self.chapter_names.keys():
+                            print(i,' : ', self.chapter_names[i])
+
+                        chapter_number = input('\n chapter number :- ') 
+                        
+                        if chapter_number  in self.chapter_list.keys():
+
+                            
+                            print("downloading {} : {}".format(chapter_number,self.chapter_names[chapter_number]))
+
+                            self._get_webpage(self.chapter_list[chapter_number])
+                            self._make_soup()
+                            self._get_image_links()
+
+                            for link in self.image_links:
+                                self._download_image(link,chapter_number)
+
+                        else:
+                            print('chapter not in list try again ... \n')
+                            os.system('cls')
+                                
+                    print('\n\n\n Download more ? [y/n] :- ')
+                    choice = input()
+
+                    if choice == 'y':
+                        os.system('cls')
+                    else:
+                        break
 
 
-                    for image_link in chapter_reader:
-                        link = str(image_link.get('src'))
-                        image_links.append(link)
 
-
-                    for link in image_links:
-                        image_data = rq.get(link,stream=True).raw
-                        filename = str(link.split('/')[-1])
-                        print('\t'+filename)
-                        final_dir_path = download_folder / managa_name / chapter_number
-                        pathlib.Path(final_dir_path).mkdir(parents=True,exist_ok=True)
-                        final_path = final_dir_path / filename
-                        image_file = open(final_path ,'wb')
-                        shutil.copyfileobj(image_data,image_file)
-                        image_file.close
-                        del image_data 
+                
         else:
             print('\n Download dir not set \nPlease enter download dir :- ')
-            self._set_download_dir(input())
+            self.set_download_dir(input())
 
 
     def _get_image_links(self):
-        pass
-    def _download_image(self):
-        pass
-    def _save_image(self):
-        pass
-    def _set_download_dir(self, directory):
-        self.download_dir = directory
+
+        chapter_reader = (self.prased_html.find(class_='container-chapter-reader')).findAll('img')
+
+        for image_link in chapter_reader:
+            link = str(image_link.get('src'))
+            self.image_links.append(link)
+
+    def _download_image(self,link,chapter_number):
+
+        self._get_webpage(link,_stream=True,_raw=True)
+        image_data = self.page
+
+        filename = str(link.split('/')[-1])
+        print('\t'+filename)
+
+        
+        final_dir_path = self.download_dir / self.name / self._slugify(str(self.chapter_names[str(chapter_number)]))
+        pathlib.Path(final_dir_path).mkdir(parents=True,exist_ok=True)
+        final_path = final_dir_path / filename
+
+        self._save_image(image_data,final_path)
+
+        del image_data 
+
+    def _save_image(self,image_data,image_file_name):
+
+        image_file = open(image_file_name ,'wb')
+        shutil.copyfileobj(image_data,image_file)
+        image_file.close
+        
+    def set_download_dir(self, directory):
+
+        self.download_dir = pathlib.Path(directory)
+    def _slugify(self,value):
+        """
+        Normalizes string, converts to lowercase, removes non-alpha characters,
+        and converts spaces to hyphens.
+        """
+        pure = ''
+        for i in value.strip().replace(' ','_'):
+            if i in string.punctuation.replace('_','').replace('(','').replace(')','').replace('.',''):
+                pure += '_'
+            else:
+                pure += i
+        return pure
